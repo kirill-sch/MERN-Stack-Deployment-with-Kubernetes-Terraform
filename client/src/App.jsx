e331347b34a100e0e262ac490bb1e7f19145d9ff
CarpetGlue <carpetglue@protonmail.com>
2024-08-15 19:46:29 +0200

    Implemented saving user login to local storage. Fixed last front card not saving.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import HomePage from './pages/HomePage'
 import Signup_Login from './pages/Signup_Login'
@@ -6,11 +6,44 @@ import Signup_Login from './pages/Signup_Login'
 function App() {
 
   const [isLoggedin, setIsLoggedin] = useState(false);
-  const [loggedInUser, setLoggedInUser] = useState(null)
+  const [loggedInUser, setLoggedInUser] = useState(null);
+
+
+  // Load logged-in user from local storage on component mount
+  useEffect(() => {
+
+    const localUser = localStorage.getItem('loggedInUserLocal');
+    if (localUser) {
+
+      setLoggedInUser(JSON.parse(localUser)); 
+      setIsLoggedin(true);
+
+    }
+  }, []); 
+
+
+  // Save logged-in user to local storage whenever it changes
+  useEffect(() => {
+
+    if (loggedInUser) {
+
+      localStorage.setItem('loggedInUserLocal', JSON.stringify(loggedInUser)); 
+      setIsLoggedin(true);
+
+    } 
+    
+    else {
+
+      localStorage.removeItem('loggedInUserLocal');
+      setIsLoggedin(false);
+    }
+  }, [loggedInUser]); 
+
+  console.log(loggedInUser);
 
   return (
     <>
-    {isLoggedin ? <HomePage setIsLoggedin={setIsLoggedin} loggedInUser={loggedInUser}/> : <Signup_Login setIsLoggedin={setIsLoggedin} setLoggedInUser={setLoggedInUser} />}
+      {isLoggedin ? <HomePage setIsLoggedin={setIsLoggedin} loggedInUser={loggedInUser} setLoggedInUser={setLoggedInUser}/> : <Signup_Login setIsLoggedin={setIsLoggedin} setLoggedInUser={setLoggedInUser} />}
     </>
   )
 }
