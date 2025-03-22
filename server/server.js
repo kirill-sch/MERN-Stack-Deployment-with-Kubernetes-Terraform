f80d75eedeff325a801f3d7352321599d7f7def5
kirill-sch <schiroky.kirill@gmail.com>
2025-03-22 21:59:47 +0100

    I don't even know

--- server/server.js
+++ server/server.js
@@ -466,4 +466,4 @@ app.get('/api/data_seed', (req, res) => {
     })
 })
 
-app.listen(3000, () => console.log('Server started on http://localhost:3000'));
+app.listen(3000, "0.0.0.0", () => console.log('Server running on port 3000'));
