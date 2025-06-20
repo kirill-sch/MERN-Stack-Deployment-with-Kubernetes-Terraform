f71d9e6fd2f54d5fb290e58095b749940789326c
kirill-sch <schiroky.kirill@gmail.com>
2025-06-20 14:44:33 +0200

    Removed kubernetes provider since it will no longer be necessary.

--- aws/provider.tf
+++ aws/provider.tf
@@ -3,11 +3,7 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
-    }
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = ">= 1.7.0"
-    }
+    }   
   }
 }
 
@@ -18,29 +14,15 @@ provider "aws" {
 data "aws_eks_cluster" "eks" {
   name = var.cluster_name
 
-  depends_on = [ 
+  depends_on = [
     aws_eks_cluster.eks-cluster
-   ]
+  ]
 }
 
 data "aws_eks_cluster_auth" "eks" {
   name = data.aws_eks_cluster.eks.name
 
-  depends_on = [ 
+  depends_on = [
     aws_eks_cluster.eks-cluster
-   ]
+  ]
 }
-
-/*provider "kubernetes" {
-  host = data.aws_eks_cluster.eks.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
-  token = data.aws_eks_cluster_auth.eks-auth.token
-} */
-
-/* provider "kubectl" {
-  host                   = data.aws_eks_cluster.eks.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.eks.token
-  load_config_file       = false
-} */
-
