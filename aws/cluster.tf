c8549591e7b939285176354189122bfe40459db3
kirill-sch <schiroky.kirill@gmail.com>
2025-07-14 16:35:50 +0200

    Cluster version from 1.30 to 1.31

--- aws/cluster.tf
+++ aws/cluster.tf
@@ -69,7 +69,7 @@ resource "aws_eks_addon" "ebs-addon" {
 resource "aws_eks_cluster" "eks-cluster" {
   name     = var.cluster_name
   role_arn = aws_iam_role.EKSClusterRole.arn
-  version  = "1.30"
+  version  = "1.31"
 
   vpc_config {
     subnet_ids         = [aws_subnet.public-subnet-1.id, aws_subnet.public-subnet-2.id, aws_subnet.private-subnet-1.id, aws_subnet.private-subnet-2.id]
